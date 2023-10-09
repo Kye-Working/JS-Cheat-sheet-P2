afd7197b916f352503c83854073c48f599a92a98
Kye-Working <kyefworkin@gmail.com>
2023-10-09 23:49:46 +0100

    ES6 Module
    - Added ES6 Module

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("TESTING...");
+console.log("\n");
+/*ES6 Module*/
+/*Importing functions, values & others from external file*/
+import { EarthsGravity, double, gravityMulitpler } from "./util.js";
+
+console.log(`Earths gravity : ${EarthsGravity}m/s²`);
+console.log(`Earths gravity doubled : ${double(EarthsGravity)}m/s²`);
+console.log(`Earth gravity x5 : ${gravityMulitpler(5, EarthsGravity)}m/s²`);
---
+++ util.js
@@ -0,0 +1,8 @@
+export const EarthsGravity = 9.807;
+
+export function double(x) {
+  return 2 * x;
+}
+export function gravityMulitpler(x, EarthsGravity) {
+  return x * EarthsGravity;
+}
